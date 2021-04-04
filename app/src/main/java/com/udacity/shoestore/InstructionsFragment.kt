40853c292763499db7b58dcbdabd235b587ec325
Simon Italia <sitalia@magicaltomato.com>
2021-04-04 21:54:57 +1000

    - major refactoring to get action bar working
    - migrated hardcoded values to string resource
    
    - added drawer menu
    
    - added instructions fragment and controller files

--- app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
+++ app/src/main/java/com/udacity/shoestore/InstructionsFragment.kt
@@ -1,12 +1,10 @@
 package com.udacity.shoestore
 
 import android.os.Bundle
-import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.databinding.DataBindingUtil
-import androidx.navigation.Navigation
-import com.udacity.shoestore.databinding.FragmentLoginBinding
-import kotlinx.android.synthetic.main.activity_main.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -15,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [LoginFragment.newInstance] factory method to
+ * Use the [InstructionsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class LoginFragment : Fragment() {
+class InstructionsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -33,26 +31,8 @@ class LoginFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         // Inflate the layout for this fragment
-        val binding: FragmentLoginBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
-
-        // set onClick listener for Button
-        binding.signIn.setOnClickListener (
-
-            //get an instance of the Navigation Controller, and set the onClickListener
-            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
-        )
-
-        binding.register.setOnClickListener (
-
-            //get an instance of the Navigation Controller, and set the onClickListener
-            Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
-        )
-
-        //show options menu
-        setHasOptionsMenu(true)
-        return binding.root //contains root of the layout just inflated above
+        return inflater.inflate(R.layout.fragment_instructions, container, false)
     }
 
     companion object {
@@ -62,16 +42,16 @@ class LoginFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment LoginFragment.
+         * @return A new instance of fragment InstructionsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            LoginFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                InstructionsFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -4,14 +4,19 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.navigateUp
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,18 +24,41 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
+        //initialize the drawer layout
+        drawerLayout = binding.drawerLayout
+
         //get Navigation Controller
         val navigationController = this.findNavController(R.id.mainNavHostFragment)
 
-        //Setup / hookup the Navigation Controller UI with the Action Bar
-//        NavigationUI.setupActionBarWithNavController(this, navigationController)
-
         //Create an appBarConfiguration with the navController.graph
-        appBarConfiguration = AppBarConfiguration(navigationController.graph)
+        appBarConfiguration = AppBarConfiguration(navigationController.graph, drawerLayout)
+
+        //Setup / hookup the Navigation Controller UI with the Action Bar
+        NavigationUI.setupActionBarWithNavController(this, navigationController, drawerLayout)
 
         //Hook up the navigation UI up to the navigation view.
-//        NavigationUI.setupWithNavController(binding.navView, navigationController)
+        NavigationUI.setupWithNavController(binding.navView, navigationController)
+
+        appDrawerListener(navigationController)
 
         Timber.plant(Timber.DebugTree())
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+
+        //get Navigation Controller
+        val navigationController = this.findNavController(R.id.mainNavHostFragment)
+        return navigateUp(navigationController, drawerLayout)
+    }
+
+    private fun appDrawerListener(navigationController: NavController) {
+        // prevent nav gesture if not on start destination
+        navigationController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _: Bundle? ->
+            if (nd.id == nc.graph.startDestination) {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
+            } else {
+                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            }
+        }
     }
 }
