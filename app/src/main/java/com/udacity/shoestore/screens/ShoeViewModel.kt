04389cee2129b7da59f14ed1a7c5765b0ccbce93
Simon Italia <sitalia@magicaltomato.com>
2021-06-06 20:46:59 +1000

    create shoe viewModel

---
+++ app/src/main/java/com/udacity/shoestore/screens/ShoeViewModel.kt
@@ -0,0 +1,38 @@
+package com.udacity.shoestore.screens
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoeViewModel: ViewModel() {
+
+    // Create a list of all desserts, in order of when they start being produced
+    private val shoeList = listOf(
+        Shoe(
+            "Casual High Top", 6.0, "Converse", "Black high top", listOf("converse")
+        ),
+
+        Shoe(
+            "Sport Sneaker", 5.0, "Adidas", "White sport sneaker", listOf("adidas")
+        ),
+
+        Shoe(
+            "Casual Sneaker ", 5.0, "Lacoste", "White casual sneaker", listOf("lacoste")
+        ),
+
+        Shoe(
+            "Canvas High Top", 5.0, "Penguin", "Navy canvas high top", listOf("penguin")
+        ),
+
+        Shoe(
+            "Casual shoe", 5.0, "Oca", "Yellow casual shoe", listOf("oca")
+        )
+    )
+
+    private val _selectedShoe = MutableLiveData<Shoe>()
+    val selectedShoe: LiveData<Shoe>
+        get() = _selectedShoe
+
+
+}
