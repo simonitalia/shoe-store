b2b8acbe430b94c859da759526a66d19c9e56ae3
Simon Italia <sitalia@magicaltomato.com>
2021-06-20 19:01:35 +1000

    refactored viewModel with live data shoe object

--- app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ShoeViewModel.kt
@@ -7,32 +7,9 @@ import com.udacity.shoestore.models.Shoe
 
 class ShoeViewModel: ViewModel() {
 
-    // Create a list of all shoes
-    private val shoeList = listOf(
-        Shoe(
-            "Casual High Top", 6.0, "Converse", "Black high top", listOf("converse")
-        ),
-
-        Shoe(
-            "Sport Sneaker", 5.0, "Adidas", "White sport sneaker", listOf("adidas")
-        ),
-
-        Shoe(
-            "Casual Sneaker ", 5.0, "Lacoste", "White casual sneaker", listOf("lacoste")
-        ),
-
-        Shoe(
-            "Canvas High Top", 5.0, "Penguin", "Navy canvas high top", listOf("penguin")
-        ),
-
-        Shoe(
-            "Casual shoe", 5.0, "Oca", "Yellow casual shoe", listOf("oca")
-        )
-    )
-
-    private val _selectedShoe = MutableLiveData<Shoe>()
-    val selectedShoe: LiveData<Shoe>
-        get() = _selectedShoe
-
+    // shoe list live data
+    private var _shoeList = MutableLiveData<MutableList<Shoe>>()
+    val shoeList : LiveData<MutableList<Shoe>>
+        get() = _shoeList //externally accessible getter
 
 }
