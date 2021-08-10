e91aeacccb5c891d1b551c307a163ed9455f4da9
Simon Italia <sitalia@magicaltomato.com>
2021-08-10 17:44:02 +1000

    remove support from app drawer throughout app

--- app/src/main/java/com/udacity/shoestore/MainActivity.kt
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -2,21 +2,16 @@ package com.udacity.shoestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.databinding.DataBindingUtil
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.navigation.ui.NavigationUI.navigateUp
 import com.udacity.shoestore.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navigationController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,23 +19,11 @@ class MainActivity : AppCompatActivity() {
 
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
 
-        //initialize the drawer layout
-        drawerLayout = binding.drawerLayout
-
         //get Navigation Controller
-        val navigationController = this.findNavController(R.id.mainNavHostFragment)
+        navigationController = this.findNavController(R.id.mainNavHostFragment)
 
-        //Create an appBarConfiguration with the navController.graph
-        appBarConfiguration = AppBarConfiguration(navigationController.graph, drawerLayout)
-
-        //Setup / hookup the Navigation Controller UI with the Action Bar
-        NavigationUI.setupActionBarWithNavController(this, navigationController, drawerLayout)
-
-        //Hook up the navigation UI up to the navigation view.
-        NavigationUI.setupWithNavController(binding.navView, navigationController)
-
-        //Set app drawer listener
-        appDrawerListener(navigationController)
+        //Setup / hookup the Navigation Controller UI
+        NavigationUI.setupActionBarWithNavController(this, navigationController)
 
         Timber.plant(Timber.DebugTree())
     }
@@ -48,18 +31,6 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
 
         //get Navigation Controller
-        val navigationController = this.findNavController(R.id.mainNavHostFragment)
-        return navigateUp(navigationController, drawerLayout)
-    }
-
-    private fun appDrawerListener(navigationController: NavController) {
-        // prevent nav gesture if not on start destination
-        navigationController.addOnDestinationChangedListener { nc: NavController, nd: NavDestination, _: Bundle? ->
-            if (nd.id == nc.graph.startDestination) {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED)
-            } else {
-                drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
-            }
-        }
+        return navigationController.navigateUp()
     }
 }
